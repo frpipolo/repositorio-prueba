1323366a73ed28425eb050aa868d16bf384440a9
Fpipolo <fpipolo.dev@gmail.com>
2022-05-26 22:41:45 -0300

    dfas

--- pichard.py
+++ pichard.py
@@ -7,10 +7,5 @@ if var1 == var2:
     print("son iguales!")
 else:
     print("no son iguales!")
-<<<<<<< HEAD
-   
- 
-=======
 
 print("esto lo escribio franco"):
->>>>>>> 8e63eb9844b3659324b68019ecbf5f91832048ce
